b94cbe3e5a574c13b81758ef3287d1f579524b1e
loebd <dorian.loeb@etu.univ-amu.fr>
2025-09-15 08:49:16 +0200

    mise en commentaire sendMail.js

--- server/utils/sendMail.js
+++ server/utils/sendMail.js
@@ -1,27 +1,27 @@
-import { MailerSend, EmailParams, Sender, Recipient } from "mailersend";
-import "dotenv/config";
+// import { MailerSend, EmailParams, Sender, Recipient } from "mailersend";
+// import "dotenv/config";
 
-export const sendMail = async (recipient, subject, html, text) => {
-  const mailerSend = new MailerSend({
-    apiKey: process.env.EMAIL_API_KEY,
-  });
+// export const sendMail = async (recipient, subject, html, text) => {
+//   const mailerSend = new MailerSend({
+//     apiKey: process.env.EMAIL_API_KEY,
+//   });
 
 
-  const sentFrom = new Sender(process.env.EMAIL_HOST, "VA Production");
+//   const sentFrom = new Sender(process.env.EMAIL_HOST, "VA Production");
 
-  const recipients = [new Recipient(recipient, "Destinataire")];
+//   const recipients = [new Recipient(recipient, "Destinataire")];
 
-  const emailParams = new EmailParams()
-    .setFrom(sentFrom)
-    .setTo(recipients)
-    .setSubject(subject)
-    .setReplyTo(sentFrom)
-    .setHtml(html)
-    .setText(text);
+//   const emailParams = new EmailParams()
+//     .setFrom(sentFrom)
+//     .setTo(recipients)
+//     .setSubject(subject)
+//     .setReplyTo(sentFrom)
+//     .setHtml(html)
+//     .setText(text);
 
-  try {
-    await mailerSend.email.send(emailParams);
-  } catch (error) {
-    console.log(error);
-  }
-};
+//   try {
+//     await mailerSend.email.send(emailParams);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
